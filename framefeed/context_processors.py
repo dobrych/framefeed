63c1a0a3cf0b225cdb5c77ad04bfd41d542ccf9d
dobrych <none@none>
2010-05-25 19:04:08 +0300

    added FRAMEFEED_ROOT_URL to template context proc

--- framefeed/context_processors.py
+++ framefeed/context_processors.py
@@ -1,4 +1,9 @@
 from django.conf import settings
+from framefeed.utils import root_url
+
 
 def settings_vars(request):
     return { 'FRAMEFEED_TITLE': getattr(settings, 'FRAMEFEED_TITLE', 'Framefeed Title'), }
+
+def cur_site(request):
+    return {'FRAMEFEED_ROOT_URL': root_url()}
